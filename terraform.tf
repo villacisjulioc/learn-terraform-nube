6445d04137323441511a6f02ddf0da31e38ab4c8
villacisjulioc <villacisjulio@yahoo.com>
2024-02-28 07:09:12 -0500

    Remove cloud block

---
+++ terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+/*
+  cloud {
+   organization = "LJR-TI"
+
+   workspaces {
+     name = "learn-terraform-nube"
+   }
+  }
+*/
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.28.0"
+    }
+  }
+
+  required_version = ">= 0.14.0"
+}
